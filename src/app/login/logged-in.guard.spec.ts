12388c519cc9f4178adc70a7c75aa0c0acb6ab77
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2017-03-13 18:16:47 +0100

    testing(loginpage): turn loginpage tests green

--- src/app/login/logged-in.guard.spec.ts
+++ src/app/login/logged-in.guard.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 import { LoggedInGuard } from './logged-in.guard';
+import {UserService} from "./user.service";
+import {Router} from "@angular/router"
+
 
 describe('LoggedInGuard', () => {
+  let userServiceSpy = jasmine.createSpyObj('UserService', ['isLoggedIn']);
+  let routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LoggedInGuard]
+      providers: [LoggedInGuard,
+        {provide: UserService, use: userServiceSpy},
+        {provide: Router, use: routerSpy}]
     });
   });
 
--- src/app/login/login-page/login-page.component.spec.ts
+++ src/app/login/login-page/login-page.component.spec.ts
@@ -1,13 +1,28 @@
 import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 import {LoginPageComponent} from "./login-page.component";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {UserService} from "../user.service";
+import {Router} from "@angular/router";
+
 
 describe('LoginPageComponent', () => {
     let component: LoginPageComponent;
     let fixture: ComponentFixture<LoginPageComponent>;
 
+    let userServiceSpy = jasmine.createSpyObj('userService', ['isLoggedIn']);
+    let routerSpy = jasmine.createSpyObj('router', ['navigateByUrl']);
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [LoginPageComponent]
+            imports: [FormsModule],
+            providers: [
+                {provide: UserService, useValue: userServiceSpy},
+                {provide: Router, useValue: routerSpy}
+            ],
+            declarations: [LoginPageComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+
         })
             .compileComponents();
     }));
