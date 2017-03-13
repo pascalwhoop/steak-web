dc331e782f34c77a9ec93ed3645678db6e6ec52b
Pascal Brokmeier <pascal.brokmeier@gmail.com>
2017-03-13 13:32:26 +0100

    testing(offers): Created OffersAPI stub

---
+++ src/testing/offers-api-stub.ts
@@ -0,0 +1,45 @@
+import {Observable} from "rxjs/Observable";
+import {Inject, Injectable, Optional} from "@angular/core";
+import {Offer} from "../app/shared/api/model/Offer";
+import {MOCK_OFFERS} from "./mock-data";
+
+
+@Injectable()
+export class OffersApiStub {
+
+    public offerDelete(username: string, offerid: string): Observable<{}> {
+        return new Observable(observer => {
+            observer.next({});
+        })
+    }
+
+
+    public offerGet(date?: Date, startdate?: Date, enddate?: Date): Observable<Array<Offer>> {
+        return new Observable(observer => {
+            observer.next(MOCK_OFFERS);
+        })
+    }
+
+
+    public offerGetOne(username: string, offerid: string): Observable<Offer> {
+        return new Observable(observer => {
+            observer.next(MOCK_OFFERS[0]);
+        })
+    }
+
+
+    public offerPUT(offerData: Offer): Observable<Offer> {
+        return new Observable(observer => {
+            observer.next(offerData);
+        })
+    }
+
+
+    public offerPost(offer: Offer): Observable<Offer> {
+        return new Observable(observer => {
+            observer.next(offer);
+        })
+    }
+
+
+}
