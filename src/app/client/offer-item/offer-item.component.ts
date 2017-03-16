e1a3f718ec6120de7f325072707c36907dff2abc
Pascal Brokmeier <pascal.brokmeier@opitz-consulting.com>
2017-03-16 21:22:24 +0100

    feature(UI): spinner added, disabling buttons on 0 orders, quick feedback

--- src/app/client/offer-item/offer-item.component.ts
+++ src/app/client/offer-item/offer-item.component.ts
@@ -1,36 +1,44 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input} from "@angular/core";
 import {Offer} from "../../shared/api/model/Offer";
 import {OrdersApi} from "../../shared/api/endpoints/OrdersApi";
 import {OfferOrdersPair} from "../../shared/api/model/OfferOrdersPair";
 import {OffersApi} from "../../shared/api/endpoints/OffersApi";
 
 @Component({
-  selector: 'steak-offer-item',
-  templateUrl: './offer-item.component.html',
-  styleUrls: ['offer-item.component.scss']
+    selector: 'steak-offer-item',
+    templateUrl: './offer-item.component.html',
+    styleUrls: ['offer-item.component.scss']
 })
 export class OfferItemComponent implements OnInit {
 
-  @Input('oo-pair')
-  ooPair: OfferOrdersPair;
+    @Input('oo-pair')
+    ooPair: OfferOrdersPair;
 
-  constructor(public orderApi: OrdersApi, public offersApi: OffersApi) { }
+    constructor(public orderApi: OrdersApi, public offersApi: OffersApi) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 
+    addOrder(offer: Offer) {
+        this.orderApi.orderPost(offer._id, false).subscribe(order => {
+            this.ooPair.orders.push(order);
+        });
+        console.log('order placed!');
 
-  addOrder(offer: Offer) {
-    this.orderApi.orderPUT(offer._id, false).subscribe(order =>{
-      this.ooPair.orders.push(order);
-    });
-    console.log('order placed!');
+    }
 
-  }
-
-  removeOrder(offer: Offer){
-
-  }
+    removeOrder(ooPair: OfferOrdersPair) {
+        //we go a reverse aproach. delete first and add again in case of error.
+        //This way, we avoid having to handle quick clicks of remove that would
+        //otherwise result in several delete orders for the same element.
+        let orderToRemove = ooPair.orders.shift();
+        if (!orderToRemove) return;
+        this.orderApi.orderDelete(orderToRemove._id)
+            .subscribe(null, error => {
+                ooPair.orders.push(orderToRemove);
+            })
+    }
 
 }
--- src/app/shared/components/working-spinner/working-spinner.component.spec.ts
+++ src/app/shared/components/working-spinner/working-spinner.component.spec.ts
@@ -1,27 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { WorkingSpinnerComponent } from './working-spinner.component';
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {WorkingSpinnerComponent} from "./working-spinner.component";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {VFeedbackService} from "../../services/vfeedback.service";
 
 describe('WorkingSpinnerComponent', () => {
-  let component: WorkingSpinnerComponent;
-  let fixture: ComponentFixture<WorkingSpinnerComponent>;
+    let component: WorkingSpinnerComponent;
+    let fixture: ComponentFixture<WorkingSpinnerComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ WorkingSpinnerComponent ],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WorkingSpinnerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [WorkingSpinnerComponent],
+            providers: [{
+                provide: VFeedbackService,
+                useValue: jasmine.createSpyObj('vfeedbackService', ['addListener', 'removeListener'])
+            }],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WorkingSpinnerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
