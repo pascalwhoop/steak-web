d2f29ba0c639ea393e22fb7c4a565e3229f76be2
Pascal Brokmeier <pascal.brokmeier@opitz-consulting.com>
2017-03-20 19:25:38 +0100

    chore(login): added test to login, excluded since not yet ready

--- src/app/login/login-page/login-page.component.spec.ts
+++ src/app/login/login-page/login-page.component.spec.ts
@@ -1,8 +1,9 @@
-import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {async, fakeAsync, ComponentFixture, TestBed} from "@angular/core/testing";
 import {LoginPageComponent} from "./login-page.component";
 import {NO_ERRORS_SCHEMA} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {UserService} from "../user.service";
+import {By} from "@angular/platform-browser";
 import {Router} from "@angular/router";
 
 
@@ -36,4 +37,27 @@ describe('LoginPageComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    xit('should call enable the login Button after entering credentials', async(() => {
+
+        fixture.whenStable()
+            .then(() => {
+                let loginButton = fixture.debugElement.query(By.css('form>button'));
+                expect(loginButton.properties['disabled']).toBe(true);
+
+                console.log(loginButton);
+
+                //simulate user entering credentials
+                let usernameInput = fixture.debugElement.query(By.css('#UsernameInput'));
+                let passwordInput = fixture.debugElement.query(By.css('#PasswordInput'));
+                usernameInput.nativeElement.value = 'testuser';
+                passwordInput.nativeElement.value = 'testpass';
+
+                fixture.detectChanges();
+
+                expect(loginButton.properties['disabled']).toBe(false);
+            })
+
+
+    }));
 });
