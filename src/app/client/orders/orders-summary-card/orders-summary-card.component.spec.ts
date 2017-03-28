529894e5e1957da8ae13f221e6961e25c63ef9b9
Pascal Brokmeier <pascal.brokmeier@opitz-consulting.com>
2017-03-28 19:04:54 +0200

    feature(orders): OrderSummaryCard showing total, expected at end of month and vegetarian average

--- src/app/client/orders/orders-summary-card/orders-summary-card.component.spec.ts
+++ src/app/client/orders/orders-summary-card/orders-summary-card.component.spec.ts
@@ -1,25 +1,84 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { OrdersSummaryCardComponent } from './orders-summary-card.component';
+import {async, ComponentFixture, TestBed, fakeAsync, tick} from "@angular/core/testing";
+import {OrdersSummaryCardComponent} from "./orders-summary-card.component";
+import {NO_ERRORS_SCHEMA, ViewChild, Component, SimpleChanges} from "@angular/core";
+import {MOCK_ORDERS} from "../../../../testing/mock-data";
+import {Order} from "../../../shared/model/Order";
 
 describe('OrdersSummaryCardComponent', () => {
-  let component: OrdersSummaryCardComponent;
-  let fixture: ComponentFixture<OrdersSummaryCardComponent>;
+    let component: OrdersSummaryCardComponent;
+    let fixture: ComponentFixture<OrdersSummaryCardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OrdersSummaryCardComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OrdersSummaryCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+            declarations: [OrdersSummaryCardComponent,TestParentComponent],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(OrdersSummaryCardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should trigger ngOnChanges on change of Input()', fakeAsync(() => {
+        //creating fake parent component
+        let fixture = TestBed.createComponent(TestParentComponent);
+        let testParentComponent = fixture.componentInstance;
+        fixture.detectChanges();
+        tick();
+        expect(testParentComponent.summaryCardChild).toBeTruthy();
+        //setting the orders to what we want to Input into our component
+        spyOn(testParentComponent.summaryCardChild, 'ngOnChanges').and.callThrough();
+        testParentComponent.orders = MOCK_ORDERS;
+        fixture.detectChanges();
+        expect(testParentComponent.summaryCardChild.ngOnChanges).toHaveBeenCalled();
+    }));
+
+
+    it('should calc sum of orders properly', () => {
+        expect(component.openSum).toBe(0);
+        callOnChangesWithMockOrders(component);
+        expect(component.openSum).toBe(10);
+    });
+
+    it('should calc vegetarian stats properly', () => {
+        expect(component.vegetarianScore).toBe(0);
+        callOnChangesWithMockOrders(component);
+        expect(component.vegetarianScore).toBe(50);
+    });
+
+    it('should calc the expected end of month costs properly', () => {
+        spyOn(component, 'getPercentageOfMonthComplete').and.returnValue(0.5);
+        callOnChangesWithMockOrders(component);
+        expect(component.expectedBill).toBe(20);
+    });
+
 });
+
+
+let callOnChangesWithMockOrders = function (component) {
+    component.ngOnChanges({orders: {currentValue: MOCK_ORDERS, previousValue: null, isFirstChange: () => true}});
+};
+
+//to test the @Input() on our component we need to do some tricking
+
+/* In the host component's template we will pass the inputs to the actual
+ * component to test, that is TestComponent in this case
+ */
+@Component({
+    selector: `test-host-component`,
+    template: `<div><steak-orders-summary-card [orders]="orders"></steak-orders-summary-card></div>`
+})
+export class TestParentComponent {
+    @ViewChild(OrdersSummaryCardComponent) /* using viewChild we get access to the OrdersSummaryCardComponent which is a child of TestHostComponent */
+    public summaryCardChild: OrdersSummaryCardComponent;
+    public orders: Order[];
+    /* this is the variable which is passed as input to the OrdersSummaryCardComponent */
+}
--- src/app/client/orders/orders-summary-card/orders-summary-card.component.ts
+++ src/app/client/orders/orders-summary-card/orders-summary-card.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, OnChanges, SimpleChanges} from "@angular/core";
+import {Order} from "../../../shared/model/Order";
 
 @Component({
-  selector: 'steak-orders-summary-card',
-  templateUrl: './orders-summary-card.component.html',
-  styleUrls: ['./orders-summary-card.component.scss']
+    selector: 'steak-orders-summary-card',
+    templateUrl: './orders-summary-card.component.html',
+    styleUrls: ['./orders-summary-card.component.scss']
 })
-export class OrdersSummaryCardComponent implements OnInit {
+export class OrdersSummaryCardComponent implements OnInit, OnChanges {
 
-  constructor() { }
+    @Input()
+    orders: Order[];
 
-  ngOnInit() {
-  }
+    openSum: number = 0;
+    vegetarianScore: number = 0;
+    expectedBill: number = 0;
 
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+
+    ngOnChanges(changes: SimpleChanges): void {
+        let newOrders = changes['orders'].currentValue;
+        if (!newOrders) return;
+        this.openSum = this.calcOpenSum(newOrders);
+        this.vegetarianScore = this.calcVegetarianScore(newOrders);
+        this.expectedBill = this.calcExpectedBill(newOrders);
+    }
+
+    private calcExpectedBill(orders: Order[]): number {
+
+        let sum = this.calcOpenSum(orders);
+        return sum / this.getPercentageOfMonthComplete();
+    }
+
+    private calcVegetarianScore(orders: Order[]): number {
+        let vegCount = orders.map(o => o.offer.vegetarian? 1 : 0).reduce((prev, curr) => prev+curr);
+        return Math.round(vegCount / orders.length * 100);
+    }
+
+    private calcOpenSum(orders: Order[]): number {
+        return orders.map(o => o.amount).reduce((prev, curr) => prev + curr);
+    }
+
+    /**
+     * Returns a percentage of the total amount of days of this month.
+     * So if its the 15th and the month has 30 days it gives back 0.5.
+     * This means that the current price is about 50% of the total expected costs this month.
+     * This ignores weekends etc but its a good estimate
+     */
+    private getPercentageOfMonthComplete(): number {
+        let d = new Date();
+        let dateNow = d.getDate();
+        let totalInMonth = this.daysInMonth(d.getMonth(), d.getFullYear());
+        return dateNow/totalInMonth;
+    }
+
+    /**
+     * Clever method taken from https://stackoverflow.com/questions/1184334/get-number-days-in-a-specified-month-using-javascript to return number of days in month
+     * @param month
+     * @param year
+     * @returns {number}
+     */
+    private daysInMonth(month, year) {
+        return new Date(year, month+1, 0).getDate();
+    }
 }
