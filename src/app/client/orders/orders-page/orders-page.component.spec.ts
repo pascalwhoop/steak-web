13988e56e50b734e7d8be71b9a0481da7b015cd4
Pascal Brokmeier <pascal.brokmeier@opitz-consulting.com>
2017-03-28 19:06:00 +0200

    feature(ordersPage): Added padding, fetching orders and passing to summaryCard

--- src/app/client/orders/orders-page/orders-page.component.spec.ts
+++ src/app/client/orders/orders-page/orders-page.component.spec.ts
@@ -1,25 +1,46 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { OrdersPageComponent } from './orders-page.component';
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {OrdersPageComponent} from "./orders-page.component";
+import {PageTitleService} from "../../../shared/services/page-title.service";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {OrdersApiStub} from "../../../../testing/orders-api-stub";
+import {OrdersApi} from "../../../shared/api/endpoints/OrdersApi";
+import Spy = jasmine.Spy;
+import {UserService} from "../../../login/user.service";
 
 describe('OrdersPageComponent', () => {
-  let component: OrdersPageComponent;
-  let fixture: ComponentFixture<OrdersPageComponent>;
+    let component: OrdersPageComponent;
+    let fixture: ComponentFixture<OrdersPageComponent>;
+    let titleService: PageTitleService;
+    let ordersSpy: Spy;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ OrdersPageComponent ]
-    })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OrdersPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [OrdersPageComponent],
+            providers: [
+                PageTitleService,
+                {provide: OrdersApi, useClass: OrdersApiStub},
+                {provide: UserService, useValue: {username: 'xxx'}}
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(OrdersPageComponent);
+        titleService = fixture.debugElement.injector.get(PageTitleService);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should set its page name to the pageTitleService', () => {
+        expect(titleService.title).toBe('Orders');
+    });
+
+    });
 });
--- src/app/client/orders/orders-page/orders-page.component.ts
+++ src/app/client/orders/orders-page/orders-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {PageTitleService} from "../../../shared/services/page-title.service";
+import {OrdersApi} from "../../../shared/api/endpoints/OrdersApi";
+import {UserService} from "../../../login/user.service";
+import {Order} from "../../../shared/model/Order";
 
 @Component({
-  selector: 'steak-orders-page',
-  templateUrl: 'orders-page.component.html',
-  styleUrls: ['orders-page.component.scss']
+    selector: 'steak-orders-page',
+    templateUrl: 'orders-page.component.html',
+    styleUrls: ['orders-page.component.scss']
 })
 export class OrdersPageComponent implements OnInit {
 
-  constructor() { }
+    orders: Order[];
 
-  ngOnInit() {
-  }
+    constructor(public title: PageTitleService, public ordersApi: OrdersApi, public userService: UserService) {
+    }
 
+    ngOnInit() {
+        this.title.title = "Orders";
+        this.fetchOffers();
+    }
+
+    private fetchOffers() : void{
+        let usr = this.userService.username;
+        this.ordersApi.ordersGET(null, null, null, usr)
+            .subscribe(offers => this.orders = offers);
+    }
 }
