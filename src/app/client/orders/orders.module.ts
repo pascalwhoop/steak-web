121a1b9b5cd4d66320343750d503712ebc1414cd
Pascal Brokmeier <pascal.brokmeier@opitz-consulting.com>
2017-03-29 11:42:36 +0200

    build(upgrade): Upgrade to Angular v4, addition of @covalent

--- src/app/client/orders/orders.module.ts
+++ src/app/client/orders/orders.module.ts
@@ -5,11 +5,13 @@ import {RouterModule} from "@angular/router";
 import {OrdersSummaryCardComponent} from "./orders-summary-card/orders-summary-card.component";
 import {OrdersHistoryTableComponent} from "./orders-history-table/orders-history-table.component";
 import {SharedModule} from "../../shared/shared.module";
+import {CovalentCoreModule} from "@covalent/core";
 
 @NgModule({
     imports: [
         CommonModule,
         SharedModule,
+        CovalentCoreModule,
         RouterModule.forChild([{path: '', component: OrdersPageComponent}])
     ],
     declarations: [OrdersPageComponent, OrdersSummaryCardComponent, OrdersHistoryTableComponent]
