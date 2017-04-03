be03f3b74378413e9839bed40485ba0fa00d79aa
Pascal Brokmeier <pascal.brokmeier@opitz-consulting.com>
2017-04-03 17:39:21 +0200

    feature(style): Showing OC Logo in drawer and top navbar

--- src/app/shared/components/main/main.component.ts
+++ src/app/shared/components/main/main.component.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 import {PageTitleService} from "../../services/page-title.service";
 import {UserService} from "../../../login/user.service";
+import {DomSanitizer} from "@angular/platform-browser";
+import {MdIconRegistry} from "@angular/material";
 
 @Component({
-  selector: 'steak-main',
-  templateUrl: 'main.component.html',
-  styleUrls: ['main.component.scss']
+    selector: 'steak-main',
+    templateUrl: 'main.component.html',
+    styleUrls: ['main.component.scss']
 })
 export class MainComponent implements OnInit {
 
-  constructor(public titleService: PageTitleService, public userService: UserService) { }
+    constructor(public titleService: PageTitleService, public userService: UserService, public iconRegistry: MdIconRegistry, public sanitizer: DomSanitizer) {
+        this.iconRegistry.addSvgIcon('oc_logo', this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/oc_logo.svg'));
+    }
 
-  getTitle() {
-    return this.titleService.title;
-  }
+    getTitle() {
+        return this.titleService.title;
+    }
 
-  getUsername(){
-    return this.userService.username;
-  }
+    getUsername() {
+        return this.userService.username;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
